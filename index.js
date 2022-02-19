1e6cac34bbdd32b7feeba9dba7b852391d5b5c2a
Joe Froula <jmfroula@gmail.com>
2022-02-19 00:03:22 -0800

    adds form validation

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-
-
-
 const occupation = document.getElementById('occupation')
+const name1 = document.getElementById('name')
+const age = document.getElementById('age')
+
+let p0 = document.getElementById('p0')
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+
+let pgraph = document.getElementById('pgraph')
+
 const form = document.getElementById('form')
 
-
 const objEvent = form.addEventListener('submit', function (e) {
-    e.preventDefault() 
+    e.preventDefault()
 
-    const name = document.getElementById('name')
-    const age = document.getElementById('age')
+    if (name1.value.length === 0 || 
+        age.value.length === 0 || 
+        occupation.value.length === 0) {
+        e.preventDefault()
+        alert('Please fill in all the fields')
+    } else 
+    
+    {
 
-    let p1 = document.getElementById('p1')
-    let p2 = document.getElementById('p2')
-    let p3 = document.getElementById('p3')
-    let p4 = document.getElementById('p4')
-    let pgraph = document.getElementById('pgraph')
-    const height = document.getElementById('height')
+        p0.innerText = 'const person = {'
+        p1.innerText = ` name: ${name1.value}`
+        p2.innerText = ` age: ${age.value}`
+        p3.innerText = ` occupation: ${occupation.value}` 
+        p4.innerText = '}'
+        pgraph.style.display = 'block'
 
-    p1.innerText = `{ name: ${name.value}`
-    p2.innerText = ` age: ${age.value}`
-    p3.innerText = ` height: ${height.value}`
-    p4.innerText = ` occupation: ${occupation.value} }`
-    pgraph.style.display = 'block'
+    }
 
 }) 
